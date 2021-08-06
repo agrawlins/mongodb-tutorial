7bb191c75c96d49b1ac66b90945fb30d0f5dcb1c
agrawlins <agrawlins@gmail.com>
2021-08-05 22:26:57 -0400

    ACTUALLY connected mongoDB to Index.js. Also created default.json to hide sensitive password information for index.js's linkup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 mongoose
-    .connect('mongodb+srv://MerrimanLyon:13qeadzc!#QEADZC@cluster0.xqosq.mongodb.net/mongoDB-tutorial?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology:true})
-    .then(() => console.log('Connected to MongoDB...'))
+    .connect(
+        config.get('mongoURI'),
+        {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => console.log('Connected to MongoDB...'))
     .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
