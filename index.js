80a7d7e77446fce7d0638a166f2275ceb2017bdb
agrawlins <agrawlins@gmail.com>
2021-08-09 18:55:00 -0400

    Last push for the tutorial

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
-const config = require('config');
+const connectDB = require('./startup/db');
+const express = require('express');
+const app = express();
+const products = require('./routes/products');
 
-mongoose
-    .connect(
-        config.get('mongoURI'),
-        {useNewUrlParser: true, useUnifiedTopology: true})
-    .then( () => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
+connectDB();
+
+app.use(express.json());
+app.use('/api/products', products);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
